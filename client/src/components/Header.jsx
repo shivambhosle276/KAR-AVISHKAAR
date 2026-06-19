92468646ab185af3d62b3e900f436502469e00d5
shivambhosle276 <shivambhosle276@gmail.com>
2026-06-19 13:34:37 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -72,7 +72,7 @@ navigate(`/search?${searchQuery}`);
         to="/"
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+        <span className="px-2 py-1 bg-gradient-to-r from-cyan-500 via-blue-500 to-violet-500 rounded-lg text-white">
           Event 
         </span>
         Management
